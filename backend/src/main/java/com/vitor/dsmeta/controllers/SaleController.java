e01751952a5d6b2cacaa0a49908c2c38f597a060
vitorsandrade <vitorsantos.dandrade@gmail.com>
2022-07-13 19:29:27 -0300

    Date select

--- backend/src/main/java/com/vitor/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/vitor/dsmeta/controllers/SaleController.java
@@ -1,10 +1,11 @@
 package com.vitor.dsmeta.controllers;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vitor.dsmeta.models.Sale;
@@ -15,11 +16,14 @@ import com.vitor.dsmeta.services.SaleService;
 public class SaleController {
 
 	@Autowired
-	private SaleService service; 
-	
+	private SaleService service;
+
 	@GetMapping
-	public List<Sale> findSales(){
-		return service.findSales();
+	public Page<Sale> findSales(
+			@RequestParam(value = "minDate", defaultValue = "") String minDate, 
+			@RequestParam(value = "maxDate", defaultValue = "")String maxDate, 
+			Pageable pageable) {
+		return service.findSales(minDate,maxDate,pageable);
 	}
-	
+
 }
